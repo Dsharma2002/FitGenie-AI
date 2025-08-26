9b34d79c138abf914901ca454fb56ca4b1a35a85
Divyansh Sharma <divyansh.abbott9712@gmail.com>
2025-08-26 14:01:51 +0100

    activity service

---
+++ README.md
@@ -0,0 +1,5 @@
+Start the backend microservice server
+
+user-service uses Postgres as the database
+activity-service uses MongoDB as the database
+
--- activityservice/src/main/java/com/fitness/activityservice/model/activity.java
+++ activityservice/src/main/java/com/fitness/activityservice/model/activity.java
@@ -3,7 +3,9 @@ package com.fitness.activityservice.model;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -14,10 +16,10 @@ import lombok.NoArgsConstructor;
 
 @Document(collection = "activities")
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class activity {
+@Builder
+public class Activity {
     @Id
     private String id;
     private String userId;
@@ -28,6 +30,10 @@ public class activity {
 
     @Field("metrics")
     private Map<String, Object> additionalMetrics;
+
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
